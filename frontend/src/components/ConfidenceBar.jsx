6b1b16ff6e34da253fd0be9a69bfe46169c48b8e
Anchithya Prabhakar <anchithyaprabhakar@gmail.com>
2026-06-26 17:12:16 +0530

    Add dynamic confidence progress bar

--- frontend/src/components/ConfidenceBar.jsx
+++ frontend/src/components/ConfidenceBar.jsx
@@ -1,16 +1,44 @@
 function ConfidenceBar({ confidence }) {
+  let color = "#22c55e"; // Green
+
+  if (confidence >= 70) {
+    color = "#ef4444"; // Red
+  } else if (confidence >= 40) {
+    color = "#f59e0b"; // Yellow
+  }
+
   return (
     <div className="info-card">
       <h3>Confidence</h3>
 
-      <div className="progress">
+      <div
+        style={{
+          width: "100%",
+          height: "12px",
+          background: "#333",
+          borderRadius: "10px",
+          overflow: "hidden",
+          marginTop: "30px",
+        }}
+      >
         <div
-          className="progress-fill"
-          style={{ width: `${confidence}%` }}
-        ></div>
+          style={{
+            width: `${confidence}%`,
+            height: "100%",
+            background: color,
+            transition: "width 0.8s ease",
+          }}
+        />
       </div>
 
-      <h2>{confidence}%</h2>
+      <h2
+        style={{
+          marginTop: "25px",
+          color,
+        }}
+      >
+        {confidence.toFixed(2)}%
+      </h2>
     </div>
   );
 }
